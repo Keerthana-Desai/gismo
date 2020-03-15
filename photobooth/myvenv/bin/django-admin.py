be2b4bf26fe6ff21d870bc21e6fa5a5976844eb8
weinmueller <pascal.weinmueller@fau.de>
2020-03-15 15:50:49 +0100

    before corona

---
+++ photobooth/myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/riemann/weinmueller/Desktop/photobooth/photobooth_django/photobooth/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
